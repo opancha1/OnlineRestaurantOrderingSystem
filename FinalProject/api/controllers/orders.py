9b13ca6e57c743eccca01f4db86fcf1107a47cfb
Dev_Patel <Dpate213@charlotte.edu>
2025-11-18 11:57:53 -0500

    Changes made by dev

--- FinalProject/api/controllers/orders.py
+++ FinalProject/api/controllers/orders.py
@@ -55,14 +55,4 @@ def update(db: Session, item_id, request):
     return item.first()
 
 
-def delete(db: Session, item_id):
-    try:
-        item = db.query(model.Order).filter(model.Order.id == item_id)
-        if not item.first():
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
-        item.delete(synchronize_session=False)
-        db.commit()
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
